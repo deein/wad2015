933c1cd3f864b58271390041facc6b1689667e29
Florian Pernot <fpernot@jktv.fr>
2015-01-15 19:52:01 +0100

    Push 6

---
+++ js/grayscale.js
@@ -0,0 +1,81 @@
+/*!
+ * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    $windowH = $(window).height();
+    if ($(".navbar").offset().top > 50) {
+        // $(".navbar-fixed-top").addClass("top-nav-collapse");
+        // $(".navbar-fixed-top").fadeIn(200);
+    } else {
+        // $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        // $(".navbar-fixed-top").fadeOut(200);
+    }
+    
+    $windowH = $(window).height();
+    if ($(".navbar.pages").offset().top) {
+        $(".navbar-fixed-top").show();
+    } else {
+        $(".navbar-fixed-top").show();
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 50
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    $('#connexion a').bind('click', function(event) {
+        $('#connexion').hide();
+        $('#motdepasse').show();
+        event.preventDefault();
+    });
+    
+    $('#motdepasse a').bind('click', function(event) {
+        $('#motdepasse').hide();
+        $('#connexion').show();
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+$('img.svg').each(function(){
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = jQuery(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+    }, 'xml');
+
+});
